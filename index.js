a5c6756b7595ba503de97d82dc6e526e19cf50ae
Eric Ingland <4283616+eingland@users.noreply.github.com>
2022-11-06 08:08:35 -0600

    Fix Timezone calculation

--- index.js
+++ index.js
@@ -8,64 +8,62 @@ const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = process.env.NOTION_DATABASE_ID
 
 async function getDoneItem() {
-  const now  =  new Date();
-    try {
-        const response = await notion.databases.query({
-            database_id: databaseId,
-            filter: {
-              and: [
-                {
-                  property: 'Done',
-                  checkbox: {
-                    equals: true,
-                  },
-                },
-                {
-                  property: 'Due',
-                  date: {
-                    on_or_before: date.format(now,'YYYY-MM-DD'),
-                  }
-                },
-                {
-                  property: 'Recur Interval',
-                  number: {
-                    greater_than_or_equal_to: 1,
-                  },  
-                },
-                {
-                  property: 'Recur Type',
-                  select: {
-                    is_not_empty: true,
-                  },  
-                },
-              ],
+  const now = new Date();
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        and: [
+          {
+            property: 'Done',
+            checkbox: {
+              equals: true,
             },
-          });
-        console.log("Retrieved tasks.");
-        return response
-    } catch (error) {
-        console.error(error.body);
-    }
+          },
+          {
+            property: 'Due',
+            date: {
+              on_or_before: date.format(now, 'YYYY-MM-DD'),
+            }
+          },
+          {
+            property: 'Recur Interval',
+            number: {
+              greater_than_or_equal_to: 1,
+            },
+          },
+          {
+            property: 'Recur Type',
+            select: {
+              is_not_empty: true,
+            },
+          },
+        ],
+      },
+    });
+    console.log("Retrieved tasks.");
+    return response
+  } catch (error) {
+    console.error(error.body);
+  }
 }
 
 async function updateItemDue(item) {
-  var oldDate = new Date(item.properties.Due.date.start);
+  var oldDateUTC = new Date(item.properties.Due.date.start);
 
-  if (item.properties['Recur Type'].select.name == "Daily")
-  {
-    var newDate = date.addDays(oldDate, item.properties['Recur Interval'].number);
-  } else if (item.properties['Recur Type'].select.name == "Weekly")
-  {
-    var newDate = date.addDays(oldDate, item.properties['Recur Interval'].number * 7);
-  } else if (item.properties['Recur Type'].select.name == "Monthly")
-  {
-    var newDate = date.addMonths(oldDate, item.properties['Recur Interval'].number);
-  } else if (item.properties['Recur Type'].select.name == "Yearly")
-  {
-    var newDate = date.addYears(oldDate, item.properties['Recur Interval'].number);
+  if (item.properties['Recur Type'].select.name == "Daily") {
+    var newDateUTC = date.addDays(oldDateUTC, item.properties['Recur Interval'].number);
+  } else if (item.properties['Recur Type'].select.name == "Weekly") {
+    var newDateUTC = date.addDays(oldDateUTC, item.properties['Recur Interval'].number * 7);
+  } else if (item.properties['Recur Type'].select.name == "Monthly") {
+    var newDateUTC = date.addMonths(oldDateUTC, item.properties['Recur Interval'].number);
+  } else if (item.properties['Recur Type'].select.name == "Yearly") {
+    var newDateUTC = date.addYears(oldDateUTC, item.properties['Recur Interval'].number);
   }
-  
-  newDate = date.addHours(newDate, process.env.TIMEZONE_HOUR_OFFSET );
+
+  // Manually add offset to time to convert to local timezone in a UTC date
+  var newDateLocal = date.addMinutes(newDateUTC, -newDateUTC.getTimezoneOffset());
+
   const response = await notion.pages.update({
     page_id: item.id,
     properties: {
@@ -74,39 +72,38 @@ async function updateItemDue(item) {
       },
       'Due': {
         date: {
-            start: newDate,
-            time_zone: process.env.TIMEZONE
-          }
+          start: newDateLocal,
+          time_zone: process.env.TZ
+        }
       },
     },
   });
-  console.log(response);
   console.log("Updated tasks.");
 }
 
 async function main() {
-  
+
   var pages = await getDoneItem();
-    if (pages)
-    {
-      var tasks = pages.results
-      if (tasks.length > 0)
-        tasks.forEach(x => updateItemDue(x));
-      else
-        console.log("No tasks to update.")
-    }
+  if (pages) {
+    var tasks = pages.results
+    if (tasks.length > 0)
+      tasks.forEach(x => updateItemDue(x));
     else
-    {
-      console.log("No response from API.")
-    }
+      console.log("No tasks to update.")
+  }
+  else {
+    console.log("No response from API.")
+  }
 }
 
-cron.schedule('*/30 * * * *', async function() {
+
+
+if (process.env.ENVIRONMENT == "Development") {
+  main();
+}
+else {
+  cron.schedule('*/30 * * * *', async function () {
     console.log('This runs every 30 minutes.');
     main();
-});
-
-if (process.env.ENVIRONMENT == "Development")
-{
-  main();
+  });
 }
